d7d6a299ca67325a289923cb447bdd689304a194
Milan Shrestha <milanshrestha17@hotmail.com>
2020-08-05 12:58:36 -0400

    Modified Satellite class to add 'warning' css class

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,13 +5,22 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    shouldShowWarning: Function;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean, shouldShowWarning: Function) {
 
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+
+        this.shouldShowWarning = function(): boolean{
+            if (this.type.toLowerCase() === 'space debris') {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
